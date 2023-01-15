7f559e399e71ddca763359c3ae5e5107490ff74e
João Artur Magalhães <joaoartur4590@gmail.com>
2023-01-14 21:59:41 -0300

    finalizado

--- front-end/tailwind.config.cjs
+++ front-end/tailwind.config.cjs
@@ -4,9 +4,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        image:
+        image: "url('./assets/background.jpg')",
+        solarPainel:
           "url('https://images.unsplash.com/photo-1558449028-b53a39d100fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80')",
-        solarPainel: "url('./src/assets/solarPainel.jpg')",
       },
     },
   },
